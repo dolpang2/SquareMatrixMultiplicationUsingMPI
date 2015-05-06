1d83c6e6e43b65542ba876d1ad1ae781c885129a
Geunho Lee <lkh7689@gmail.com>
2015-05-06 19:31:41 +0900

    Extract Class and erase redundant variables

--- SquareMatrixMultiplicationUsingMPI/src/com/tistory/iostream/Main.java
+++ SquareMatrixMultiplicationUsingMPI/src/com/tistory/iostream/Main.java
@@ -15,49 +15,57 @@ public class Main {
    * @param args
    */
   public static void main(String[] args) {
-    int N = 1500;
-    int numtasks, /* number of tasks in partition */
+    runMultiplication(args);
+  }
+
+  /**
+   * @param args
+   */
+  private static void runMultiplication(String[] args) {
+    int sizeOfMatrix = 1500;
+    int numOfTasks, /* number of tasks in partition */
     taskid, /* a task identifier */
     numworkers, /* number of worker tasks */
     source, /* task id of message source */
-    dest, /* task id of message destination */
     mtype, /* message type */
     averow, extra, /* used to determine rows sent to each worker */
 
     /* misc */
     count;
-    int[] a = new int[N * N]; /* matrix A to be multiplied */
-    int[] b = new int[N * N]; /* matrix B to be multiplied */
-    int[] c = new int[N * N]; /* result matrix C */
+
+    SquareMatrix matrixA = new SquareMatrix(sizeOfMatrix);
+    SquareMatrix matrixB = new SquareMatrix(sizeOfMatrix);  
+
+    int[] c = new int[sizeOfMatrix * sizeOfMatrix]; /* result matrix C */
+
     int[] offset = new int[1];
     int[] rows = new int[1]; /* rows of matrix A sent to each worker */
 
     long[] computeTime = new long[1];
     long[] maxComputeTime = new long[1];
+
+
+
     MPI.Init(args);
     taskid = MPI.COMM_WORLD.Rank();
-    numtasks = MPI.COMM_WORLD.Size();
-    numworkers = numtasks - 1;
+    numOfTasks = MPI.COMM_WORLD.Size();
+    numworkers = numOfTasks - 1;
 
+    int[] convertedMatrixA = matrixA.getMatrix();
+    int[] convertedMatrixB = matrixB.getMatrix();
     /* *************** Master Task ****************** */
     if (taskid == MessageTag.MASTER.getValue()) {
-      // Init matrix A,B
-      for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-          a[(i * N) + j] = 1;
-          b[(i * N) + j] = 2;
-        }
-      }
+      
 
       // Send matrix data to worker tasks
       long start = System.currentTimeMillis();
-      averow = N / numworkers;
-      extra = N % numworkers;
+      averow = sizeOfMatrix / numworkers;
+      extra = sizeOfMatrix % numworkers;
       offset[0] = 0;
       mtype = MessageTag.FROM_MASTER.getValue();
 
       long startsend = System.currentTimeMillis();
-      for (dest = 1; dest <= numworkers; dest++) {
+      for (int dest = 1; dest <= numworkers; dest++) {
         if (dest <= extra) {
           rows[0] = averow + 1;
         } else {
@@ -65,10 +73,10 @@ public class Main {
         }
         MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, dest, mtype);
         MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, dest, mtype);
-        count = rows[0] * N;
-        MPI.COMM_WORLD.Send(a, (offset[0] * N), count, MPI.INT, dest, mtype);
-        count = N * N;
-        MPI.COMM_WORLD.Send(b, 0, count, MPI.INT, dest, mtype);
+        count = rows[0] * sizeOfMatrix;
+        MPI.COMM_WORLD.Send(convertedMatrixA, (offset[0] * sizeOfMatrix), count, MPI.INT, dest, mtype);
+        count = sizeOfMatrix * sizeOfMatrix;
+        MPI.COMM_WORLD.Send(convertedMatrixB, 0, count, MPI.INT, dest, mtype);
         offset[0] = offset[0] + rows[0];
       }
       long stopsend = System.currentTimeMillis();
@@ -81,8 +89,8 @@ public class Main {
         System.out.println("Rank " + i + " uses " + computeTime[0] + " for computing");
         MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, source, mtype);
         MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, source, mtype);
-        count = rows[0] * N;
-        MPI.COMM_WORLD.Recv(c, offset[0] * N, count, MPI.INT, source, mtype);
+        count = rows[0] * sizeOfMatrix;
+        MPI.COMM_WORLD.Recv(c, offset[0] * sizeOfMatrix, count, MPI.INT, source, mtype);
       }
       long stop = System.currentTimeMillis();
       // System.out.println("Result of matrix c[0] = " + c[0] + ", c[1000*1000] = " + c[100*100]);
@@ -96,17 +104,18 @@ public class Main {
       source = MessageTag.MASTER.getValue();
       MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, source, mtype);
       MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, source, mtype);
-      count = rows[0] * N;
-      MPI.COMM_WORLD.Recv(a, 0, count, MPI.INT, source, mtype);
-      count = N * N;
-      MPI.COMM_WORLD.Recv(b, 0, count, MPI.INT, source, mtype);
+      count = rows[0] * sizeOfMatrix;
+      MPI.COMM_WORLD.Recv(convertedMatrixA, 0, count, MPI.INT, source, mtype);
+      count = sizeOfMatrix * sizeOfMatrix;
+      MPI.COMM_WORLD.Recv(convertedMatrixB, 0, count, MPI.INT, source, mtype);
 
       long startCompute = System.currentTimeMillis();
       for (int i = 0; i < rows[0]; i++) {
-        for (int k = 0; k < N; k++) {
-          c[(i * N) + k] = 0;
-          for (int j = 0; j < N; j++) {
-            c[(i * N) + k] = c[(i * N) + k] + a[(i * N) + j] * b[(j * N) + k];
+        for (int k = 0; k < sizeOfMatrix; k++) {
+          c[(i * sizeOfMatrix) + k] = 0;
+          for (int j = 0; j < sizeOfMatrix; j++) {
+            c[(i * sizeOfMatrix) + k] =
+                c[(i * sizeOfMatrix) + k] + convertedMatrixA[(i * sizeOfMatrix) + j] * convertedMatrixB[(j * sizeOfMatrix) + k];
           }
         }
       }
@@ -116,7 +125,8 @@ public class Main {
       MPI.COMM_WORLD.Send(computeTime, 0, 1, MPI.LONG, MessageTag.MASTER.getValue(), mtype);
       MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, MessageTag.MASTER.getValue(), mtype);
       MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, MessageTag.MASTER.getValue(), mtype);
-      MPI.COMM_WORLD.Send(c, 0, rows[0] * N, MPI.INT, MessageTag.MASTER.getValue(), mtype);
+      MPI.COMM_WORLD.Send(c, 0, rows[0] * sizeOfMatrix, MPI.INT, MessageTag.MASTER.getValue(),
+          mtype);
     }
 
     MPI.COMM_WORLD.Reduce(computeTime, 0, maxComputeTime, 0, 1, MPI.LONG, MPI.MAX, 0);
@@ -124,7 +134,6 @@ public class Main {
       System.out.println("Max compute time/machine = " + maxComputeTime[0]);
     }
     MPI.Finalize();
-
   }
 
 }
--- SquareMatrixMultiplicationUsingMPI/src/com/tistory/iostream/SquareMatrix.java
+++ SquareMatrixMultiplicationUsingMPI/src/com/tistory/iostream/SquareMatrix.java
@@ -3,10 +3,45 @@
  */
 package com.tistory.iostream;
 
+import java.util.Random;
+
 /**
  * @author Lee
  *
  */
 public class SquareMatrix {
+  private int[] matrix;
+  private int sizeOfMatrix;
 
+  /**
+   * @param size: Square Matrix Size
+   */
+  public SquareMatrix(int sizeOfMatrix) {
+    super();
+    this.sizeOfMatrix = sizeOfMatrix;
+    matrix = new int[sizeOfMatrix * sizeOfMatrix];
+    
+    initializeMatrix();
+  }
+
+  /**
+   *
+   */
+  private void initializeMatrix() {
+    final int bound = 5;
+    Random randomGenerator = new Random();
+    
+    for (int i = 0; i < sizeOfMatrix; ++i) {
+      for (int j = 0; j < sizeOfMatrix; ++j) {
+        matrix[(i * sizeOfMatrix) + j] = randomGenerator.nextInt(bound);
+      }
+    }
+  }
+
+  /**
+   * @return the matrix
+   */
+  public int[] getMatrix() {
+    return matrix;
+  }
 }
